d361ef4e40a642a6124c818bf871a98ab005c0b1
cd-mbc <gitsource@cd-mbc.jp>
2018-08-20 23:54:02 +0900

    パッケージのパス指定を変更

--- access/access.go
+++ access/access.go
@@ -1,77 +1,72 @@
-
 package access
 
-
 import (
-  "fmt"
-  "strconv"
-  "io/ioutil"
-  "encoding/json"
-  "net/url"
-  
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"strconv"
+
 	"github.com/dghubble/oauth1"
 
-  "../model"
-  "../setting"
+	"github.com/cd-mbc/twitter_api_cli_go/model"
+	"github.com/cd-mbc/twitter_api_cli_go/setting"
 )
 
-
 func Base_access(url string) []byte {
-  config := oauth1.NewConfig(setting.CK, setting.CS)
-  token := oauth1.NewToken(setting.AT, setting.AS)
-  httpClient := config.Client(oauth1.NoContext, token)
+	config := oauth1.NewConfig(setting.CK, setting.CS)
+	token := oauth1.NewToken(setting.AT, setting.AS)
+	httpClient := config.Client(oauth1.NoContext, token)
 
-  res, err := httpClient.Get(url)
-  if err != nil {
-    return nil
-  }
-  defer res.Body.Close()
+	res, err := httpClient.Get(url)
+	if err != nil {
+		return nil
+	}
+	defer res.Body.Close()
 
-  body, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    return nil
-  }
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil
+	}
 
-  return body
+	return body
 }
 
 func Search_tweets(q string, count int) *model.Tweets {
-  path := setting.Search_tweets_path
-  params := url.Values{}
-  params.Add("q", q)
-  params.Add("count", strconv.Itoa(count) )
+	path := setting.Search_tweets_path
+	params := url.Values{}
+	params.Add("q", q)
+	params.Add("count", strconv.Itoa(count))
 
-  body := Base_access( path + params.Encode() )
-  if body == nil {
-    return nil
-  }
+	body := Base_access(path + params.Encode())
+	if body == nil {
+		return nil
+	}
 
-  var tweets model.Tweets
-  if err := json.Unmarshal(body, &tweets); err != nil {
-    return nil
-  }
-  fmt.Println(tweets.Search_metadata.Max_id)
+	var tweets model.Tweets
+	if err := json.Unmarshal(body, &tweets); err != nil {
+		return nil
+	}
+	fmt.Println(tweets.Search_metadata.Max_id)
 
-  return &tweets
+	return &tweets
 }
 
-
 func Get_trends(id int) *[]model.Trends {
-  path := setting.Get_trends_path
-  params := url.Values{}
-  params.Add("id", strconv.Itoa(id) )
+	path := setting.Get_trends_path
+	params := url.Values{}
+	params.Add("id", strconv.Itoa(id))
 
+	body := Base_access(path + params.Encode())
+	if body == nil {
+		return nil
+	}
 
-  body := Base_access( path + params.Encode() )
-  if body == nil {
-    return nil
-  }
+	var trends []model.Trends
+	if err := json.Unmarshal(body, &trends); err != nil {
+		fmt.Println(err)
+		return nil
+	}
 
-  var trends []model.Trends
-  if err := json.Unmarshal(body, &trends); err != nil {
-    fmt.Println(err)
-    return nil
-  }
-
-  return &trends
+	return &trends
 }
--- main.go
+++ main.go
@@ -1,82 +1,78 @@
-
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "html/template"
+	"fmt"
+	"html/template"
+	"net/http"
 
-  "./access"
-  "./model"
-  "./database"
-  "./setting"
+	"github.com/cd-mbc/twitter_api_cli_go/access"
+	"github.com/cd-mbc/twitter_api_cli_go/database"
+	"github.com/cd-mbc/twitter_api_cli_go/model"
+	"github.com/cd-mbc/twitter_api_cli_go/setting"
 )
 
-
 func get_trends_from_db() []model.Trend {
-  trend_seq_db := database.Select_trend_all()
-  return trend_seq_db
+	trend_seq_db := database.Select_trend_all()
+	return trend_seq_db
 }
 
 func trends_db_update(trends model.Trends) []model.Trend {
-  trend_seq := trends.Trends
-  if len(trend_seq) != 0 {
-    database.Delete_trend_all()
-  }
+	trend_seq := trends.Trends
+	if len(trend_seq) != 0 {
+		database.Delete_trend_all()
+	}
 
-  for i := 0; i < len(trend_seq); i++ {
-    fmt.Println(trend_seq[i].Name)
-    database.Insert_trend(&trend_seq[i])
-  }
+	for i := 0; i < len(trend_seq); i++ {
+		fmt.Println(trend_seq[i].Name)
+		database.Insert_trend(&trend_seq[i])
+	}
 
-  return trend_seq
+	return trend_seq
 }
 
 func get_tweet_of_trend(name string) []model.Tweet {
-  tweets := access.Search_tweets(name, setting.Tweet_max)
-  if tweets != nil {
-    return tweets.Statuses
-  } else {
-    return []model.Tweet{}
-  }
+	tweets := access.Search_tweets(name, setting.Tweet_max)
+	if tweets != nil {
+		return tweets.Statuses
+	} else {
+		return []model.Tweet{}
+	}
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	woeid := setting.Woeid_japan
 
-func index(w http.ResponseWriter, r *http.Request){
-  woeid := setting.Woeid_japan
+	trends := access.Get_trends(woeid)
+	trend_seq := []model.Trend{}
+	if trends != nil {
+		trend_seq = trends_db_update((*trends)[0])
+	} else {
+		trend_seq = get_trends_from_db()
+	}
 
-  trends := access.Get_trends(woeid)
-  trend_seq := []model.Trend{}
-  if trends != nil {
-    trend_seq = trends_db_update( (*trends)[0] )
-  } else {
-    trend_seq = get_trends_from_db()
-  }
-
-  t, _ := template.ParseFiles("./template/index.html")
-  t.Execute(w, trend_seq)
+	t, _ := template.ParseFiles("./template/index.html")
+	t.Execute(w, trend_seq)
 }
 
-func trend_tweet(w http.ResponseWriter, r *http.Request){
-  r.ParseForm()
-  query := r.Form
-  name := query["name"][0]
-  tweet_seq := get_tweet_of_trend(name)
+func trend_tweet(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	query := r.Form
+	name := query["name"][0]
+	tweet_seq := get_tweet_of_trend(name)
 
-  t, _ := template.ParseFiles("./template/trend_tweet.html")
-  t.Execute(w, tweet_seq)
+	t, _ := template.ParseFiles("./template/trend_tweet.html")
+	t.Execute(w, tweet_seq)
 }
-
 
 func main() {
 
-  database.Init_db()
+	database.Init_db()
 
-  server := http.Server{
-    Addr: setting.Host + ":" + setting.Port,
-  }
-  http.HandleFunc("/", index)
-  http.HandleFunc("/trend_tweet", trend_tweet)
-  server.ListenAndServe()
+	server := http.Server{
+		Addr: setting.Host + ":" + setting.Port,
+	}
+	http.HandleFunc("/", index)
+	http.HandleFunc("/trend_tweet", trend_tweet)
+	server.ListenAndServe()
 
 }
